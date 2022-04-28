178dd8706dfcbc2738a7ba307eac2022a5b36734
brewchetta <chett.tiller@gmail.com>
2022-04-28 17:00:47 -0400

    moved dom manipulators to index, deferred scripts, puts globals at top of page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
+// VARIABLES ///////////////////////////////////////////////////////////////
+
+const commentsContainer = document.getElementById("commentsContainer")
+
+const form = document.getElementById("identicon-form")
+form.addEventListener("submit", handleSubmit)
+
+const commentForm = document.getElementById("comment-form")
+form.addEventListener("submit", newComment)
+
+// DOM MANIPULATION ///////////////////////////////////////////////////////////////
+
+function addComment(comment) {
+  let p = document.createElement("p")
+  p.innerText = comment
+  commentsContainer.appendChild(p)
+}
+
+function updateComments(comments) {
+  comments.map(function(c) {
+    addComment(c)
+  })
+}
+
+// EVENT HANDLERS ///////////////////////////////////////////////////////////////
+
 function handleSubmit(e) {
   e.preventDefault();
   inputString = e.target[0].value
   identicon = new Identicon(inputString);
-  updateGravatar(identicon)
   loadComments(inputString)
 }
 
@@ -12,12 +37,13 @@ function loadComments(gravatar) {
     .then(resp => {
       comments = resp.map(comment => comment.content)
       updateComments(comments)
-    })
+    }
+  )
 }
 
 function newComment(e) {
-  comment = e.target.value
-  gravatar = document.getElementById("identicon-form")[0].value
+  const comment = e.target.value
+  const gravatar = document.getElementById("identicon-form")[0].value
 
   fetch(`http://localhost:3000/comments`, {
     method: 'POST',
@@ -33,10 +59,3 @@ function newComment(e) {
 
   addComment(comment)
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("identicon-form")
-  form.addEventListener("submit", handleSubmit)
-  const commentForm = document.getElementById("comment-form")
-  form.addEventListener("submit", newComment)
-})
