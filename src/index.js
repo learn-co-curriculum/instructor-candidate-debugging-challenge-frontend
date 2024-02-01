cfe1c498938b8012c4805b6b7a56b164a0032ff9
a0k03l3 <ashok.sangwan@icanbwell.com>
2024-01-31 18:30:10 -0800

    fixed the bugs

--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ let form = document.getElementById("identicon-form")
 form.addEventListener("submit", handleSubmit)
 
 let commentForm = document.getElementById("comment-form")
-form.addEventListener("submit", newComment)
+commentForm.addEventListener("submit", newComment)
 
 
 function addComment(comment) {
+  if(comment){
   let p = document.createElement("p")
   p.innerText = comment
   commentsContainer.appendChild(p)
+  }
 }
 
 function updateComments(comments) {
@@ -25,6 +27,7 @@ function handleSubmit(e) {
   let inputString = e.target[0].value
   let identicon = new Identicon(inputString);
   loadComments(inputString)
+
 }
 
 function loadComments(gravatar) {
@@ -32,17 +35,20 @@ function loadComments(gravatar) {
     .then(resp => resp.json())
     .then(resp => {
       let comments = resp.map(comment => comment.content)
+        
       updateComments(comments)
+      
     }
   )
 }
 
 
 function newComment(e) {
-  // WHY ISN'T THIS WORKING???
-  let comment = e.target.value
+ e.preventDefault();
+ 
+  let comment = e.target[0].value
   let gravatar = document.getElementById("identicon-form")[0].value
-
+  e.target[0].value=""
   fetch(`http://localhost:3000/comments`, {
     method: 'POST',
     headers: {
